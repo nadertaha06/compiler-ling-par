96ac320810a7ccbe8a2ba75e0e34d1f6168d8a15
nadertaha06 <tahanader068@gmail.com>
2026-08-11 12:10:41 -0300

    Roteiro 1 - v0.0

---
+++ main.ts
@@ -0,0 +1,70 @@
+function main(args: string): number{
+    let num = ""; // if (args[i] == '+' || args[i] == '-' )
+    const nums: number[] = [];
+    const ops: string[] = [];
+    for (let i = 0; i < args.length;i++){
+        if (  '0' <= args[i] && args[i]  <= '9'){
+            num += args[i];
+        }
+        else{
+            if (num != ''){
+                nums.push(Number(num))
+                num = ""
+                
+            }
+
+            switch (args[i]) {
+                case '+':
+                case '-':
+                    ops.push(args[i]);
+                    break;
+                case ' ':
+                    break;
+                default:
+                    throw new Error("Caractere invalido");
+            }
+            /*
+            if (args[i] == '+' || args[i] == '-' ){
+                ops.push(args[i])
+                break;
+            }
+            if (args[i] === ' ' || args[i] == ''){
+                break;
+            }
+            else{
+                throw new Error("Os numeros nao foram enviados");
+            }*/
+            
+
+        }
+    }
+    if (num != ''){
+        nums.push(Number(num))
+        num = ""
+    }
+    if (ops.length +1 != nums.length){
+        throw new Error("Os numeros nao foram enviados");
+    }
+
+
+
+    let soma = nums[0];
+    for (let j = 0; j < ops.length;j++){
+        
+        if(ops[j] === '+'){
+            soma += nums[j + 1 ] 
+        }
+        else{
+            soma -= nums[j+1]
+        }
+
+    }
+    
+
+
+
+    return soma;
+}
+
+
+console.log(main(process.argv[2]))
