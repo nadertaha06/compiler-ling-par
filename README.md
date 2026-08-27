cd000257c56a3d0b053dac850a117288b3ec5600
nadertaha06 <tahanader068@gmail.com>
2026-08-27 12:45:31 -0300

    Roteiro 3 - v1.1

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 [![Compilation Status](https://compiler-tester.insper-comp.com.br/svg/nadertaha06/compiler-ling-par)](https://compiler-tester.insper-comp.com.br/svg/nadertaha06/compiler-ling-par)
 
-![Diagrama Sintático](https://compiler-tester.insper-comp.com.br/ds?version=v1.0)
+![Diagrama Sintático](https://compiler-tester.insper-comp.com.br/ds?version=v1.1)
+
+​```ebnf
+EXPRESSION = TERM, { ("+" | "-"), TERM } ;
+TERM = FACTOR, { ("*" | "/"), FACTOR } ;
+FACTOR = ("+" | "-"), FACTOR | "(", EXPRESSION, ")" | NUMBER ;
+NUMBER = DIGIT, {DIGIT} ;
+DIGIT = 0 | 1 | ... | 9 ;
+​```
--- main.ts
+++ main.ts
@@ -27,6 +27,26 @@ class Lexer {
         this.position = this.position + 1;
         return;
       }
+      if ( c == "*"){
+        this.next = new Token("MULTI","*");
+        this.position = this.position +1;
+        return;
+      }
+      if (c == "/"){
+        this.next = new Token("DIV","/");
+        this.position = this.position +1;
+        return;
+      }
+      if (c == "("){
+        this.next = new Token("OPEN_PAR","(");
+        this.position = this.position +1;
+        return;
+      }
+      if (c == ")"){
+        this.next = new Token("CLOSE_PAR",")");
+        this.position = this.position +1;
+        return;
+      }
       if (c >= "0" && c <= "9" ){
         let sum = "";
         while ( this.position < this.source.length && (this.source[this.position] >= "0" && this.source[this.position] <= "9" )){
@@ -42,19 +62,56 @@ class Lexer {
 class Parser{
   static lexer: Lexer;
   static parseExpression(): number {
-    if (Parser.lexer.next.type != "INT")throw new Error("[Parser] Expected INT");
-    let resultado = Number(Parser.lexer.next.value);
-    Parser.lexer.selectNext()
+    let resultado = Parser.parseTerm()
     while (Parser.lexer.next.type == "PLUS" || Parser.lexer.next.type == "MINUS" ){
       let op = Parser.lexer.next.type;
       Parser.lexer.selectNext()
-      if (Parser.lexer.next.type != "INT") throw new Error("[Parser] Expected INT after operator");
-      if (op == "PLUS") resultado += Number(Parser.lexer.next.value);
-      if (op == "MINUS") resultado -= Number(Parser.lexer.next.value);
-      Parser.lexer.selectNext()
+      if (op == "PLUS") resultado += Parser.parseTerm()
+      if (op == "MINUS") resultado -= Parser.parseTerm()
     }
     return resultado;
 
+  }
+  static parseTerm(): number{
+    let resultado = Parser.parseFactor();
+    while (Parser.lexer.next.type == "MULTI" || Parser.lexer.next.type == "DIV" ){
+      let op = Parser.lexer.next.type;
+      Parser.lexer.selectNext()
+      if (op == "MULTI") resultado *= Parser.parseFactor();
+      if (op == "DIV" ) {
+        let value = Parser.parseFactor();
+        if (value == 0 ) throw new Error("[Parser] Division by zero");
+        resultado = Math.trunc(resultado / value);
+      }
+    }
+    return resultado;
+    
+  }
+  static parseFactor(): number{
+    if (Parser.lexer.next.type  == "INT"){
+      let t = Number(Parser.lexer.next.value);
+      Parser.lexer.selectNext()
+      return t;
+    }
+    if (Parser.lexer.next.type == "OPEN_PAR"){
+      Parser.lexer.selectNext();
+      //if (Parser.lexer.next.type != "INT") throw new Error("[Parser] Expected INT after operator");
+      let resultado = Parser.parseExpression();
+      if (Parser.lexer.next.type != "CLOSE_PAR") throw new Error("[Parser] Par opend with now close");
+      Parser.lexer.selectNext();
+      return resultado;
+    }
+    if (Parser.lexer.next.type == "MINUS"){
+      Parser.lexer.selectNext();
+      let valor = Parser.parseFactor();
+      return -Number(valor);
+    }
+    if (Parser.lexer.next.type == "PLUS"){
+      Parser.lexer.selectNext();
+      let valor = Parser.parseFactor();
+      return Number(valor);
+    }
+    throw new Error("[Parser] Expected INT or ( after operator");
   }
   static run(code: string): number{
     Parser.lexer = new Lexer(code);
